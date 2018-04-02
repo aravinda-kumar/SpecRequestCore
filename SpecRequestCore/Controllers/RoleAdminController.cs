1bc01e4959a66300bd04f1c94899341b2dfe8c92
theFalconator <falcon.nick@gmail.com>
2018-04-02 18:28:07 -0500

    added (GET) edit action, viewmodel, view for role administration

---
+++ SpecRequestCore/Controllers/RoleAdminController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SpecRequestCore.Models;
+using SpecRequestCore.Models.RoleAdminViewModels;
+
+namespace SpecRequestCore.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
+
+        public RoleAdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index() => View(roleManager.Roles);
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            var users = userManager.Users.ToList();
+            var members = new List<ApplicationUser>();
+            var nonMembers = new List<ApplicationUser>();
+
+            foreach (var user in users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    members.Add(user);
+                }
+                nonMembers.Add(user);
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ SpecRequestCore/Models/RoleAdminViewModels/EditRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SpecRequestCore.Models.RoleAdminViewModels
+{
+    public class EditRoleViewModel
+    {
+        public IdentityRole Role { get; set; }
+
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+   }
+}
