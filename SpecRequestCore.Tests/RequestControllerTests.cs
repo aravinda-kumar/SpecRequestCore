5644410fe09340433d6aeef854e46b45f1c601dc
theFalconator <falcon.nick@gmail.com>
2018-04-07 09:58:50 -0500

    cleanup login view and home index

--- SpecRequestCore.Tests/RequestControllerTests.cs
+++ SpecRequestCore.Tests/RequestControllerTests.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SpecRequestCore.Controllers;
 using SpecRequestCore.Repositories;
@@ -25,17 +27,20 @@ namespace SpecRequestCore.Tests
                         Id = 1,
                         Name = "Section 744",
                         Description = "Fix adlfgkhqwoetihqehnioin12i3o5u qeoruh12 35jksbdfoih o1235ohasdjlnag",
-                        RequestCreated = DateTime.Now
+                        RequestCreated = DateTime.Now,
+                        UserId = "123-abc" // fake guid
                     }
                 }).AsQueryable<Request>());
 
             var controller = new RequestController(mock.Object, null);
 
-            var result = controller.Index().ViewData.Model as IEnumerable<Request>;
+            var result = controller.Index().Result as ViewResult;
+            
+            var model = result.ViewData.Model as IEnumerable<Request>;
 
-            Assert.Single(result);
-            Assert.Equal(1, result.First().Id);
-            Assert.Equal("Section 744", result.First().Name);
+            Assert.Single(model);
+            Assert.Equal(1, model.First().Id);
+            Assert.Equal("Section 744", model.First().Name);
         }
     }
 }
--- SpecRequestCore/Controllers/RequestController.cs
+++ SpecRequestCore/Controllers/RequestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using SpecRequestCore.Repositories;
 
 namespace SpecRequestCore.Controllers
 {
+    [Authorize]
     public class RequestController : Controller
     {
         private IRequestRepository repository;
